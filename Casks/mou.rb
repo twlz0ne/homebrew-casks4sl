d20f5c2fffed3d455c79d025284841138e041785
gongqijian <gongqijian@gmail.com>
2013-11-04 21:30:45 +0800

    Mou 0.6.6

---
+++ Casks/mou.rb
@@ -0,0 +1,7 @@
+class Mou < Cask
+  url 'http://mouapp.com/download/Mou_0.6.6.zip'
+  homepage 'http://mouapp.com/'
+  version '0.6.6'
+  sha1 'dd9ec9fef37b032a2104f10d17e39686a449ca2e'
+  link 'Mou.app'
+end
